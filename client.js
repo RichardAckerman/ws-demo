a0c4071d8879e395e098967f078f7fe05cfa6382
davoschen <davoschen@xiaoman.com>
2025-02-20 10:58:34 +0800

    feat: 改为es module

--- client.js
+++ client.js
@@ -1,31 +1,31 @@
-import('socket.io-client').then((module) => {
-  const io = module.default
-  const socket = io('http://localhost:9556', {
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:9556', {
     transports: ['websocket'],  // 只使用 websocket
     reconnection: true,
     reconnectionAttempts: 5,
     reconnectionDelay: 1000
-  });
-  // 接收消息
-  socket.on("connect", function open() {
+});
+
+// 接收消息
+socket.on("connect", function open() {
     console.log("已连接到服务器，ID:", socket.id);
     socket.emit("client_data", "Hello, server!"); // 向服务器发送消息
-  });
+});
 
-  socket.on("server_data", function incoming(data) {
+socket.on("server_data", function incoming(data) {
     console.log("服务端发来的消息：", data); // 打印从服务器接收到的消息
-  });
+});
 
-  socket.on("connect_error", (res) => {
+socket.on("connect_error", (res) => {
     console.log("connect_error", res); // 打印从服务器接收到的消息
-  });
+});
 
-  socket.on("disconnect", () => {
+socket.on("disconnect", () => {
     console.log("与服务器断开连接");
-  });
+});
 
-  setTimeout(() => {
+setTimeout(() => {
     socket.emit("client_data", "Hello, server!"); // 向服务器发送消息
-  }, 1000);
-})
+}, 1000);
 
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express');
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
+const http = createServer(app);
+const io = new Server(http, {
     path: '/socket.io/',
     cors: {
         origin: "*",
@@ -14,8 +22,8 @@ const io = require('socket.io')(http, {
     pingTimeout: 60000,
     pingInterval: 25000
 });
+
 const host = 'localhost', port = 9556;
-const path = require('path');
 
 app.use('/ws', express.static(path.join(__dirname)));
 app.use(express.static('public'));
@@ -27,18 +35,17 @@ http.listen(port, () => {
 io.on('connection', (socket) => {
     const id = socket.id;
     console.log('客户端已连接，ID:', id);
-    console.log('当前连接数:',io.sockets.sockets.size)
+    console.log('当前连接数:', io.sockets.sockets.size);
 
     socket.on('client_data', (data) => {
         const time = new Date().toLocaleString();
         console.log('收到客户端数据--：', data);
-        console.log('当前连接数:',io.sockets.sockets.size)
+        console.log('当前连接数:', io.sockets.sockets.size);
         socket.emit('server_data', `服务端已收到数据: ${time} --- ${data}`);
     });
 
     socket.on('disconnect', () => {
-        // 移除断开连接的客户端
         console.log('客户端断开连接，ID:', socket.id);
-        console.log('当前连接数:',io.sockets.sockets.size)
+        console.log('当前连接数:', io.sockets.sockets.size);
     });
-})
+});
